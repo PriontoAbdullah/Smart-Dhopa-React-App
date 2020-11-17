b8592df8233e465a72583b4590ba3af95548a86f
PriontoAbdullah <priontosoft@gmail.com>
2020-11-17 20:10:15 +0600

    User Registration & Login Done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthProvider, PrivateRoute } from './components/Authentication/useAuth';
+import Login from './components/Authentication/Login';
 import Header from './components/Header/Header';
 import About from './components/Home/About';
 import ChooseUs from './components/Home/ChooseUs';
 import ContactUs from './components/Home/ContactUs';
 import Footer from './components/Home/Footer';
-import Hero from './components/Home/Hero'; 
+import Hero from './components/Home/Hero';
 import Review from './components/Home/Review';
 import Services from './components/Home/Services';
 import Works from './components/Home/Works';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <About />
-      <Services />
-      <Works />
-      <ChooseUs />
-      <Review />
-      <ContactUs />
-      <Footer />
-    </>
-  );
+	return (
+		<div>
+			<AuthProvider>
+				<Router>
+					<Switch>
+						<Route exact path="/">
+							<Header />
+							<Hero />
+							<About />
+							<Services />
+							<Works />
+							<ChooseUs />
+							<Review />
+							<ContactUs />
+							<Footer />
+						</Route>
+
+						<Route path="/login">
+							<Login />
+						</Route>
+					</Switch>
+				</Router>
+			</AuthProvider>
+		</div>
+	);
 }
 
 export default App;
