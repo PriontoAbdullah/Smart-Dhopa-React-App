c8a0896015757690d5f9d3090696f56c401d1f42
codepundit1 <jahid35-266@diu.edu.bd>
2020-11-28 20:56:16 +0600

    I have completed dry cleaning service section.

---
+++ src/fakeData/dryCleaning/dryCleaningHouse.js
@@ -0,0 +1,104 @@
+const dryCleaningHouse = [
+	{
+		id: '4301',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Blanket / Quilt / Comforter (Single)',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 300,
+		quantity: 1
+	},
+	{
+		id: '4302',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Blanket / Quilt / Comforter (Double)',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 400,
+		quantity: 1
+	},
+	{
+		id: '4303',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Bed Sheet(Single)',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 80,
+		quantity: 1
+	},
+	{
+		id: '4304',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Bed Sheet(Double)',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 160,
+		quantity: 1
+	},
+	{
+		id: '4305',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Door Mat',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 90,
+		quantity: 1
+	},
+	{
+		id: '4306',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Sofa Cover',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 120,
+		quantity: 1
+	},
+	{
+		id: '4307',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Carpet',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 30,
+		quantity: 1
+	},
+	{
+		id: '4308',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Towel',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 50,
+		quantity: 1
+	},
+	{
+		id: '4309',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Pillow',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 100,
+		quantity: 1
+	},
+	{
+		id: '4310',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Travel Bag',
+		category: 'Household',
+		service: 'Dry Cleaning',
+		price: 250,
+		quantity: 1
+	}
+];
+
+export default dryCleaningHouse;
