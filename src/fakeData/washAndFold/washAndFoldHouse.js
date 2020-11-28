2592f1abaa2c4a561142ea806fb5dc8286fab92f
anisurarzu <62845237+anisurarzu@users.noreply.github.com>
2020-11-28 22:05:58 +0600

    wash&fold updated

---
+++ src/fakeData/washAndFold/washAndFoldHouse.js
@@ -0,0 +1,199 @@
+const washAndFoldHa = [
+	{
+		id: '2301',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Bed Sheet',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 80,
+		quantity: 1
+	},
+	{
+		id: '2302',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Pillow Case',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 40,
+		quantity: 1
+	},
+
+	{
+		id: '2303',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Towel',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 50,
+		quantity: 1
+	},
+
+	{
+		id: '2304',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Quilt',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 350,
+		quantity: 1
+	},
+
+	{
+		id: '2305',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Blanket (Single)',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 300,
+		quantity: 1
+	},
+
+	{
+		id: '2306',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Blanket (Double)',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 380,
+		quantity: 1
+	},
+
+	{
+		id: '2307',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Blanket Cover / Bed Cover',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 120,
+		quantity: 1
+	},
+
+	{
+		id: '2308',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Table Cover',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+	},
+
+	{
+		id: '2309',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Curtain (Per ft Width)',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 18,
+		quantity: 1
+	},
+
+	{
+		id: '2310',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Sofa Cover',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+	},
+
+	{
+		id: '2311',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Chair / Cushion Cover',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+	},
+
+	{
+		id: '2312',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Cushion / Pillow',
+		category: 'Household',
+		service: 'Wash And Fold',
+		price: 100,
+		quantity: 1
+	},
+
+	{
+		id: '2313',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Floor / Prayer Mat',
+		category: 'Household',
+		service: 'Wash And Fold',
+		price: 55,
+		quantity: 1
+	},
+
+	{
+		id: '2314',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Doll (Big)',
+		category: 'Household',
+		service: 'Wash And Fold',
+		price: 320,
+		quantity: 1
+	},
+
+	{
+		id: '2315',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Doll(Small)',
+		category: 'Household',
+		service: 'Wash And Fold',
+		price: 200,
+		quantity: 1
+	},
+
+	{
+		id: '2316',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Car Seat Cover',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 100,
+		quantity: 1
+	},
+
+	{
+		id: '2317',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Accessories (Prayer Cap / Napkin)',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+	},
+
+	{
+		id: '2318',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Travel Bag',
+		category: 'Household ',
+		service: 'Wash And Fold',
+		price: 280,
+		quantity: 1
+	}
+];
+export default washAndFoldHa;
---
+++ src/fakeData/washAndFold/washAndFoldWomen.js
@@ -0,0 +1,222 @@
+const washAndFoldWomen = [
+	{
+		id: '2201',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Top Wear (Shirt / T-shirt / Fatua)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+    },
+    
+	{
+		id: '2202',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Bottom Wear (Trouser / Slack / Pazama)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+    },
+    
+	{
+		id: '2203',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Accessories (Scarf / Dupatta / Orna / Muffler)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+    },
+    
+	{
+		id: '2204',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Winter Wear (Sweater / Cardigan)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 85,
+		quantity: 1
+    },
+    
+	{
+		id: '2205',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Home Wear (Nightdress / Normal Maxi)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+    },
+    
+	{
+		id: '2206',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Long Dress(Borka / Jumpsuit / Arabian Dress)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 60,
+		quantity: 1
+    },
+    
+	{
+		id: '2207',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Inner Wear (Blous / Petticoat / Under Garments)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+    },
+    
+	{
+		id: '2208',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Saree (Plain Cotton / Plain Silk / Cotton Design)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 125,
+		quantity: 1
+    },
+    
+	{
+		id: '2209',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Saree - Exclusive (Benarasi / Jamdani / Katan)',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 200,
+		quantity: 1
+    },
+    
+	{
+		id: '2210',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Kameez / Kaftan / Apron / Tops',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 45,
+		quantity: 1
+    },
+    
+	{
+		id: '2211',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Exclusive Kameez',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 80,
+		quantity: 1
+    },
+    
+	{
+		id: '2212',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Waist Coat / Coati',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 60,
+		quantity: 1
+    },
+    
+	{
+		id: '2213',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Skirt',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 80,
+		quantity: 1
+    },
+    
+	{
+		id: '2214',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Kameez',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 130,
+		quantity: 1
+    },
+    
+	{
+		id: '2215',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Dress / Gown',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 250,
+		quantity: 1
+    },
+    
+	{
+		id: '2216',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Skirt Suit',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 200,
+		quantity: 1
+    },
+    
+	{
+		id: '2217',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Shawl',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 100,
+		quantity: 1
+    },
+    
+	{
+		id: '2218',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Blazer / Coat / Jacket',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 100,
+		quantity: 1
+    },
+    
+	{
+		id: '2219',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Leather Jacket',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 450,
+		quantity: 1
+    },
+    
+	{
+		id: '2220',
+        ac: 'd-block',
+        dc: 'd-none',
+		name: 'Overcoat',
+		category: "Women's wear",
+		service: 'Wash And Fold',
+		price: 300,
+		quantity: 1
+	}
+];
+export default washAndFoldWomen;
